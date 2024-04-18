9626061476256347a0b4580a8345b1e75c932cdc
Allan <allanfermin51@gmail.com>
2024-04-18 19:51:44 -0300

    Criação do Card com estilização (CSS) e estrutura (HTML)

--- front-end-project/src/App.jsx
+++ front-end-project/src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      hello world
+      <div className="card">
+
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="" /> 
+        
+        </div>
     </>
   )
 }
